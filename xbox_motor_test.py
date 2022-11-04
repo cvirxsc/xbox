311fcbd5744f030d5a1580fb15068c6a72b30e29
Joey <102666935+cvirxsc@users.noreply.github.com>
2022-11-04 13:23:12 +0800

    Update xbox_motor_test.py

--- xbox_motor_test.py
+++ xbox_motor_test.py
@@ -13,7 +13,8 @@ while not done:
     for event_ in pygame.event.get():
         if event_.type == pygame.JOYAXISMOTION or event_.type ==pygame.JOYHATMOTION:
             try:
-                joystick.rumble(high_frequency=10,low_frequency=1,duration=0.5)
+                joystick.rumble(high_frequency=10,low_frequency=1,duration=0)
+                #官方文档给出，duration为0的时候为持续震动，需要用stop_rumble()函数打断施法，具体参数有待测试。
             except KeyboardInterrupt:
                 print("无法通过脚本控制电机")
 pygame.quit()
